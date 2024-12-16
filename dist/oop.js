3ed170ead9731937aaf4c42b1956ae7b229c61c0
SundayYogurt <kritsanalalu5@gmail.com>
2024-12-16 12:24:18 +0700

    oop2

--- dist/oop.js
+++ dist/oop.js
@@ -4,6 +4,9 @@ class Customer {
         this.name = name;
         this.address = address;
     }
+    getInfo() {
+        return "Name: " + this.name + "\nAddress: " + this.address;
+    }
 }
 class Order {
     constructor(customer, date, status) {
@@ -13,53 +16,98 @@ class Order {
         this.date = date;
         this.status = status;
     }
-    calcSub() {
+    calcSubtotal() {
+        let subtotal = 0;
+        ;
+        for (let i = 0; i < this.orderDetails.length; i++) {
+            subtotal = this.orderDetails[i].calcSubTotal();
+        }
+        return subtotal;
     }
     calcTax() {
+        let vat = 0;
+        for (let i = 0; i < this.orderDetails.length; i++) {
+            vat = vat + this.orderDetails[i].calcTax();
+        }
+        return vat;
     }
     calcTotal() {
+        return this.calcSubtotal() + this.calcTax();
     }
     calcTotalWeight() {
+        let weight = 0;
+        for (let i = 0; i < this.orderDetails.length; i++) {
+            weight = weight + this.orderDetails[i].calcWeight();
+        }
     }
     addOrderDetail(orderDetails) {
         this.orderDetails.push(orderDetails);
     }
     payOrder(payment) {
+        this.payment = payment;
+    }
+    getPayment() {
+        return this.payment;
+    }
+    printOrderDetail() {
+        for (let i = 0; i < this.orderDetails.length; i++) {
+            this.orderDetails[i].printDetail();
+        }
     }
 }
 class OrderDetail {
-    constructor(quantity, taxStatus) {
+    constructor(item, quantity, taxStatus) {
+        this.item = item;
         this.quantity = quantity;
         this.taxStatus = taxStatus;
     }
     calcSubTotal() {
+        return this.quantity * this.item.getPriceForQuantity();
     }
     calcWeight() {
+        return this.quantity * this.item.getShippingWeight();
     }
     calcTax() {
+        if (this.taxStatus === "not included") {
+            return this.item.getTax() * this.item.getTax();
+        }
+        return 0;
+    }
+    printDetail() {
+        console.log(this.item.getName() + "\t" + this.quantity + "  (ชิ้น)\t" + this.calcSubTotal() + "฿");
     }
 }
 class Item {
-    constructor(shippingWeight, description) {
+    constructor(shippingWeight, description, price) {
         this.shippingWeight = shippingWeight;
+        this.price = price;
         this.description = description;
     }
     getPriceForQuantity() {
+        return this.price;
     }
     getTax() {
+        return this.price * 0.07;
     }
     inStock() {
+        return true;
+    }
+    getInfo() {
+        return "Name:" + this.description + ", Price:" + this.price + "฿, Weigth:" + this.shippingWeight + " kg.";
+    }
+    getShippingWeight() {
+        return this.shippingWeight;
+    }
+    getName() {
+        return this.description;
     }
 }
 class Payment {
     constructor(amount) {
         this.amount = amount;
     }
-}
-class Cash extends Payment {
-    constructor(amount, cashTendered) {
-        super(amount);
-        this.cashTendered = cashTendered;
+    getAmount() {
+        return this.amount;
     }
 }
 class Check extends Payment {
@@ -69,6 +117,18 @@ class Check extends Payment {
         this.bankID = bankID;
     }
     authorized() {
+    }
+}
+class Cash extends Payment {
+    constructor(amount, cashTendered) {
+        super(amount);
+        this.cashTendered = cashTendered;
+    }
+    getChange() {
+        return this.cashTendered - this.getAmount();
+    }
+    getCashTendered() {
+        return this.cashTendered;
     }
 }
 class Credit extends Payment {
@@ -81,3 +141,35 @@ class Credit extends Payment {
     authorized() {
     }
 }
+//create objects
+//customer
+const customer = new Customer("Mr.Choke Dee", "85 Malaiman road, Nakornpathom");
+console.log(customer.getInfo());
+//Items
+const item1 = new Item(1.5, "Lotus water", 15);
+console.log(item1.getInfo());
+const item2 = new Item(0.05, "Lays", 30);
+console.log(item2.getInfo());
+const item3 = new Item(0.6, "mama", 15);
+console.log(item3.getInfo());
+//order 
+const order1 = new Order(customer, "16/12/2567", "in progress");
+//Order Detail
+const orderdetail1 = new OrderDetail(item1, 1, "not included");
+const orderdetail2 = new OrderDetail(item2, 2, "not included");
+const orderdetail3 = new OrderDetail(item3, 500, "not included");
+//OrderDetail => Order
+order1.addOrderDetail(orderdetail1);
+order1.addOrderDetail(orderdetail2);
+order1.addOrderDetail(orderdetail3);
+let amount = order1.calcTotal();
+const cash = new Cash(amount, 1000000);
+order1.payOrder(cash);
+console.log("#################### Order ####################");
+console.log(order1.printOrderDetail());
+console.log("vat =" + order1.calcTax());
+console.log("total =" + order1.calcSubtotal());
+console.log("recive =" + order1.getPayment().getCashTendered());
+console.log("Change: " + order1.getPayment().getChange());
+console.log(customer.getInfo());
+console.log("#################### Order ####################");
--- src/oop.ts
+++ src/oop.ts
@@ -3,9 +3,13 @@ class Customer {
     private name:string;
     private address:string;
 
+
     constructor(name:string, address:string){
         this.name = name;
         this.address = address;
+    }
+        public getInfo():string{
+        return "Name: "+ this.name + "\nAddress: "+ this.address;
     }
 }
 
@@ -16,95 +20,140 @@ class Order {
     private date:string;
     private status:string;
 
+
+
     constructor(customer:Customer,date:string,status:string){
         this.customer = customer;
         this.date = date;
         this.status = status;
     }
 
-    public calcSub(){
-
+    public calcSubtotal(){
+        let subtotal = 0;
+;        for(let i = 0; i < this.orderDetails.length; i++){
+            subtotal = this.orderDetails[i].calcSubTotal()
+        }
+        return subtotal;
     }
+
+
 
     public calcTax(){
+        let vat = 0;
+        for(let i =0; i < this.orderDetails.length; i++){
+            vat = vat +this.orderDetails[i].calcTax();
+        }
+        return vat;
+    }
+
+    public calcTotal():number {
+        return this.calcSubtotal() + this.calcTax();
 
     }
 
-    public calcTotal(){
-
-    }
 
     public calcTotalWeight(){
-
+        let weight = 0;
+        for(let i =0; i < this.orderDetails.length; i++){
+            weight = weight +this.orderDetails[i].calcWeight();
+        }
     }
     public addOrderDetail(orderDetails: OrderDetail){
         this.orderDetails.push(orderDetails)
     }
     public payOrder(payment: Payment){
-        
+        this.payment=payment
+    }
+
+    public getPayment(){
+        return this.payment;
+    }
+    public printOrderDetail():void{
+        for(let i = 0; i< this.orderDetails.length;i++){
+        this.orderDetails[i].printDetail();
     }
 }
-
+}
 
 class OrderDetail{
     private quantity:number;
     private taxStatus:string;
+    private item: Item;
 
-    constructor(quantity:number, taxStatus:string){
+
+    constructor(item:Item ,quantity:number, taxStatus:string){
+        this.item = item;
         this.quantity = quantity;
         this.taxStatus = taxStatus; 
         
     }
     
     public calcSubTotal(){
-  
+        return this.quantity*this.item.getPriceForQuantity();
+        
     }
 
     public calcWeight(){
-
+        return this.quantity * this.item.getShippingWeight();
     }
 
     public calcTax(){
-        
+        if(this.taxStatus === "not included"){
+            return this.item.getTax() * this.item.getTax() ;
+        }
+        return 0;
+    }
+
+    public printDetail():void {
+        console.log(this.item.getName()+ "\t"+this.quantity +"  (ชิ้น)\t"+this.calcSubTotal()+"฿");
     }
 }
 
 class Item {
     private shippingWeight: number;
     private description: string;
+    private price:number;
 
-    constructor(shippingWeight:number, description: string){
+    constructor(shippingWeight: number,description:string, price:number){
         this.shippingWeight = shippingWeight;
-        this.description =description;
+        this.price = price;
+        this.description = description;
     }
 
     public getPriceForQuantity(){
-
+        return this.price;
     }
 
-    public getTax(){
-
+    public getTax(){   
+        return this.price * 0.07;
     }
 
     public inStock(){
+        return true;
+    }
 
+     public getInfo():string{
+        return "Name:"+ this.description+", Price:"+this.price +"฿, Weigth:"+this.shippingWeight+" kg.";
+    }
+
+
+    public getShippingWeight(){
+        return this.shippingWeight;
+    }
+
+    public getName(){
+        return this.description;
     }
 }
 
-class Payment {
+abstract class Payment {
     public amount:number;
 
     constructor (amount:number){
         this.amount = amount;
     }
-}
-
-class Cash extends Payment{
-    public cashTendered: number;
-
-    constructor(amount:number ,cashTendered: number){
-        super(amount);
-        this.cashTendered = cashTendered;
+    public getAmount():number{
+        return this.amount;
     }
 }
 
@@ -121,6 +170,23 @@ class Check extends Payment{
     }
   }
 
+class Cash extends Payment{
+    public cashTendered: number;
+
+    constructor(amount:number ,cashTendered: number){
+        super(amount);
+        this.cashTendered = cashTendered;
+    }
+public getChange():number{
+    return this.cashTendered - this.getAmount();
+  }
+  public getCashTendered(): number{
+    return this.cashTendered;
+  }
+}
+
+
+
 class Credit extends Payment{
     public number:string;
     public type:string;
@@ -134,4 +200,55 @@ class Credit extends Payment{
     }
     public authorized(){
     }
+
   }
+
+
+
+  //create objects
+
+  //customer
+  const customer = new Customer("Mr.Choke Dee","85 Malaiman road, Nakornpathom")
+  console.log(customer.getInfo());
+
+  //Items
+
+  const item1 = new Item(1.5,  "Lotus water",15);
+  console.log(item1.getInfo())
+  const item2 = new Item(0.05,"Lays",30,);
+  console.log(item2.getInfo())
+  const item3 = new Item(0.6,"mama",15,);
+  console.log(item3.getInfo())
+
+  //order 
+  const order1 = new Order(customer,"16/12/2567", "in progress");
+
+  //Order Detail
+
+  const orderdetail1 = new OrderDetail(item1,1, "not included")
+
+  const orderdetail2 = new OrderDetail(item2,2, "not included")
+
+   const orderdetail3 = new OrderDetail(item3,500, "not included")
+
+  //OrderDetail => Order
+
+  order1.addOrderDetail(orderdetail1);
+  order1.addOrderDetail(orderdetail2);
+  order1.addOrderDetail(orderdetail3);
+
+
+  let amount = order1.calcTotal();
+  const cash = new Cash(amount, 1000000)
+
+  order1.payOrder(cash);
+
+
+  console.log("#################### Order ####################")
+console.log(order1.printOrderDetail())
+console.log("vat =" + order1.calcTax())
+console.log("total =" + order1.calcSubtotal())
+console.log("recive =" + (order1.getPayment()as Cash).getCashTendered());
+ console.log("Change: " +  (order1.getPayment()as Cash).getChange());
+  console.log(customer.getInfo());
+   console.log("#################### Order ####################")
